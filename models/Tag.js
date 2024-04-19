20fc8ddc3cd70344939c31ba2ba82151933a5df4
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-04-19 18:48:54 +0100

    defined the tag model columns

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,18 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // 3. define columns
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrament: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
+
   },
   {
     sequelize,
