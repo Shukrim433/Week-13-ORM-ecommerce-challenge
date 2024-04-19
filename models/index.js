80f40fa8308879d3e0ab2d41b108344c74bd4217
Shukri <150862418+Shukrim433@users.noreply.github.com>
2024-04-19 19:20:12 +0100

    established the relationships between the models

--- models/index.js
+++ models/index.js
@@ -4,13 +4,34 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+//one to many
+// Categories have many Products: 1 category has many products
+Category.hasMany(Product,{
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
+                                                                      // } "category_id" is the foreign key that links these two models together
+// Products belongsTo Category: each product belongs to 1 category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
-// Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+//many to many
+// Products belongToMany Tags (through ProductTag): 1 product can have multiple tags associated with it***
+Product.belongsToMany(Tag, {
+  through: ProductTag,            // Use through for many-to-many associations
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
+})
+
+// Tags belongToMany Products (through ProductTag):  1 tag can be associated with multiple products***
+Tag.belongsToMany(Product, {
+  through: ProductTag,            // Use through for many-to-many associations
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE',
+})
 
 module.exports = {
   Product,
@@ -18,3 +39,11 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+
+/*** example, if you have a product like "T-shirt," it might be associated with tags like "clothing," "fashion," and "apparel." */
+/*** example, the tag "clothing" might be associated with various products like "T-shirt," "pants," and "jackets." */
+
+/***ProductTag model acts as a junction or pivot table that facilitates this many-to-many relationship. It contains foreign keys that reference
+the id columns of both the Product and Tag tables. This setup allows for efficient querying and management of the relationships between products 
+and tags. */
